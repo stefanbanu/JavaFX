16a85ccb1b992b8b995bcf5a6055e40dca209f76
Stefan Banu <stefanbanu1@gmail.com>
2015-01-19 09:36:19 +0200

    test

--- TestFX/src/application/Main.java
+++ TestFX/src/application/Main.java
@@ -20,6 +20,7 @@ public class Main extends Application {
 	            @Override
 	            public void handle(ActionEvent event) {
 	                System.out.println("Hello World!");
+	                // test
 	            }
 	        });
 	        
